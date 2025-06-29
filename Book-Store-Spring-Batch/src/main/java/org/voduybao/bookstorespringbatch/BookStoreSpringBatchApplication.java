c5858f45ce0587626d6c98e766655a3c665614cd
VoDuyBao <voduybao053@gmail.com>
2025-06-29 15:13:38 +0700

    done

---
+++ Book-Store-Spring-Batch/src/main/java/org/voduybao/bookstorespringbatch/BookStoreSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package org.voduybao.bookstorespringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreSpringBatchApplication.class, args);
+	}
+
+}
